1592ae4729582392c42f30a50bcc0192ea392a31
Phan Duc Anh <pdanh.work@gmail.com>
2024-08-16 23:20:23 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,48 @@
+var monitorInput = document.querySelector(".monitor-input");
+var monitorResult = document.querySelector(".monitor-result");
+var numberBtns = document.querySelectorAll(".number");
+var spermBtns = document.querySelectorAll(".sperm");
+var str = "";
+var result = 0;
+var crrNum = 0;
+var prevNum = 0;
+var crrN = "";
+var checkSperm = false;
+const regex = /.*([+\-X/]=)$/gm;
+var checkEqualsBtn = false;
+numberBtns.forEach((b) => {
+  b.onclick = function (e) {
+    var num = e.target.innerText;
+    str += num;
+    monitorInput.innerText = str;
+  };
+});
+spermBtns.forEach((b) => {
+  b.onclick = function (e) {
+    var sperm = e.target.innerText;
+    if (sperm === "=") {
+      result = eval(str);
+      monitorResult.innerText = result;
+    }
+    str += sperm;
+    monitorInput.innerText = str;
+  };
+});
+document.querySelector(".reset").onclick = function () {
+  if (confirm("Sure ?")) {
+    str = "";
+    monitorInput.innerText = "";
+    monitorResult.innerText = "";
+  }
+};
+Object.assign(monitorInput.style, {
+  heigth: "50%",
+  color: "black",
+  fontSize: "40px",
+});
+Object.assign(monitorResult.style, {
+  height: "50%",
+  color: "black",
+  fontSize: "40px",
+  textAlign: "end",
+});
